6bc15fa988499ec569fe4a5e3f9c8500c8322d78
Mark Gomersbach <markgomersbach@gmail.com>
2018-10-31 23:47:51 +0100

    Move to Travis CI
    Signed-off-by: Mark Gomersbach <markgomersbach@gmail.com>

---
+++ .travis/setup_repoman.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+sudo chmod a+rwX /etc/passwd /etc/group /etc /usr
+echo "portage:x:250:250:portage:/var/tmp/portage:/bin/false" >> /etc/passwd
+echo "portage::250:portage,travis" >> /etc/group
+
+mkdir -p /etc/portage/repos.conf /var/db/repos/${OVERLAY_NAME}
+mv * /var/db/repos/${OVERLAY_NAME}/
+mv .git /var/db/repos/${OVERLAY_NAME}/
+git clone https://git.gomersbach.nl/mgomersbach/gentoo-mirror.git /usr/portage/
+cp .travis/gentoo.conf /etc/portage/repos.conf/
+cp .travis/${OVERLAY_NAME}.conf /etc/portage/repos.conf/
+mkdir -p /usr/portage/metadata/{dtd,xml-schema}
+wget -O /usr/portage/metadata/dtd/metadata.dtd https://www.gentoo.org/dtd/metadata.dtd
+wget -O /usr/portage/metadata/xml-schema/metadata.xsd https://www.gentoo.org/xml-schema/metadata.xsd
+
+mkdir /tmp/portage && cd /tmp/portage
+wget -qO - "https://gitweb.gentoo.org/proj/portage.git/snapshot/portage-${PORTAGE_VER}.tar.gz" | tar xz
