49d410c6fd28b7e53d1e91ac1824706f53d265f4
Mark Gomersbach <markgomersbach@gmail.com>
2018-11-01 00:37:33 +0100

    Move to Travis CI make .travis/setup_repoman.sh executable execute .travis/setup_repoman.sh with sudo privileges limit clone depth of portage mirror to 1 move sudo to travis yaml

--- .travis/setup_repoman.sh
+++ .travis/setup_repoman.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-sudo chmod a+rwX /etc/passwd /etc/group /etc /usr
+sudo mkdir /var/db/repos
+sudo chmod a+rwX /etc/passwd /etc/group /etc /usr /var/db/repos
 echo "portage:x:250:250:portage:/var/tmp/portage:/bin/false" >> /etc/passwd
 echo "portage::250:portage,travis" >> /etc/group
 
 mkdir -p /etc/portage/repos.conf /var/db/repos/${OVERLAY_NAME}
-mv * /var/db/repos/${OVERLAY_NAME}/
+mv !(${OVERLAY_NAME}) /var/db/repos/${OVERLAY_NAME}/
 mv .git /var/db/repos/${OVERLAY_NAME}/
 git clone --depth 1 https://git.gomersbach.nl/mgomersbach/gentoo-mirror.git /usr/portage/
 cp .travis/gentoo.conf /etc/portage/repos.conf/
