b64e91bfc406b8405563bce68d92ffb702b9f4e2
Mark Gomersbach <markgomersbach@gmail.com>
2018-11-01 09:17:24 +0100

    Move to Travis CI make .travis/setup_repoman.sh executable execute .travis/setup_repoman.sh with sudo privileges limit clone depth of portage mirror to 1 move sudo to travis yaml Global env vars Make dirs with sudo and parents Escape overlay name in interpreter

--- .travis/setup_repoman.sh
+++ .travis/setup_repoman.sh
@@ -8,7 +8,7 @@ echo "portage:x:250:250:portage:/var/tmp/portage:/bin/false" >> /etc/passwd
 echo "portage::250:portage,travis" >> /etc/group
 
 mkdir -p /etc/portage/repos.conf /var/db/repos/${OVERLAY_NAME}
-mv !(${OVERLAY_NAME}) /var/db/repos/${OVERLAY_NAME}/
+mv !("${OVERLAY_NAME}") /var/db/repos/${OVERLAY_NAME}/
 mv .git /var/db/repos/${OVERLAY_NAME}/
 git clone --depth 1 https://git.gomersbach.nl/mgomersbach/gentoo-mirror.git /usr/portage/
 cp .travis/gentoo.conf /etc/portage/repos.conf/
